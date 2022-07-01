93fc056a8faff3dacf759cb9db356d57106fa5eb
Nikolai Lopin <nikolai.lopin@mytaxi.com>
2022-07-01 19:42:19 +0200

    add Select component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,14 +5,51 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
-const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
+const Wrapper = styled.div`
+  position: relative;
+  padding: 8px 16px;
+  border-radius: 8px;
+  border: none;
+  background-color: ${COLORS.transparentGray15};
+  width: fit-content;
+  
+  &:focus-within {
+    outline: 5px dotted black;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+`
 
-  return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
-  );
+const InvisibleSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  appearance: none;
+`
+
+const DisplayedValue = styled.div`
+    padding-right: 32px;
+`
+
+const ArrowIcon = styled(Icon).attrs(()=> ({id: 'chevron-down'}))`
+  position: absolute;
+  right: 8px;
+  top: 5px;
+`
+
+const Select = ({ label, value, onChange, children }) => {
+    const displayedValue = getDisplayedValue(value, children);
+    return (
+        <Wrapper>
+            <DisplayedValue>{displayedValue}</DisplayedValue>
+            <ArrowIcon/>
+            <InvisibleSelect value={value} onChange={onChange}>
+                {children}
+            </InvisibleSelect>
+        </Wrapper>
+    );
 };
 
 export default Select;
